b83367f8abfacfaa20cdf3f09453f17b4e8119e5
Suraj Yadav <136949446+suraj18yad9@users.noreply.github.com>
2023-07-02 12:18:33 +0530

    Add files via upload
    Portfolio website

---
+++ portfolio_web/main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    String:["Frontend Developer" ,"Web Developer " ,"Graphic Designer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+
+})
